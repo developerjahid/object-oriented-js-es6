8654612a104652615283dcde9571858f9fcaee4a
Developer Jahid <jhpabel@gmail.com>
2020-04-30 17:15:08 +0600

    This Keyword

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,30 @@
-//Object Literal with Methods
+//This Keyword
 let rect = {
     width: 100,
     height: 50,
     draw: function() {
         console.log('I am a rectangle')
-        console.log('My width is ' + this.width)
+        this.printProperties()
+        console.log(this)
+    },
+    printProperties: function() {
+        console.log('My width is ' + this.weight)
         console.log('My height is ' + this.height)
     }
 }
 
 rect.draw()
 
-rect.width = 150
+function myFunc() {
+    console.log(this)
+}
 
-rect.draw()
+new myFunc
+
+var another = {
+    weight: 10,
+    height: 20,
+    print: rect.printProperties
+}
+
+another.print()
