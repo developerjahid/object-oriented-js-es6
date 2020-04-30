c4a0553177f24bf4d302168a9ca040fa06909052
Developer Jahid <jhpabel@gmail.com>
2020-04-30 20:14:37 +0600

    Class using Constructor

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,16 @@
-//Class using factory pattern
-var createRect = function(width, height) {
-    return {
-        width: width,
-        height: height,
-        draw: function() {
-            console.log('I am a rectangle')
-            this.printProperties()
-        },
-        printProperties: function() {
-            console.log('My width is ' + this.width)
-            console.log('My height is ' + this.height)
-        }
+//Class using Constructor
+var Rectangle = function(width, height) {
+    this.width = width
+    this.height = height
+    this.draw = function() {
+        console.log('I am a rectangle')
+        this.printProperties()
+    }
+    this.printProperties = function() {
+        console.log('My width is ' + this.width)
+        console.log('My height is ' + this.height)
     }
 }
 
-var rect2 = createRect(6, 8)
-
-rect2.draw()
-
-var rect3 = createRect(5, 393)
-
+var rect3 = new Rectangle(4, 4)
 rect3.draw()
