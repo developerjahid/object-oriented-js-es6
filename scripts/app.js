db86815574f482a887be98925db92c1f8e778491
Developer Jahid <jhpabel@gmail.com>
2020-05-01 12:38:00 +0600

    Inheritence + es6 start

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,7 @@
-//apply, call, bind
-function test(c, d) {
-    console.log(this)
-    console.log(this.a + this.b + c + d)
+//Inheritance
+function Person(name) {
+    this.name = name
 }
 
-//Call, Apply
-test.call({a: 4, b: 5}, 2, 1)
-test.apply({a: 4, b: 5}, [2, 1])
-
-//bind
-var testBind = test.bind({a: 4, b: 5}, 2, 1)
-testBind()
-
-//bind
-var testBind2 = test.bind({a: 4, b: 5})
-testBind2(5, 8)
+let p1 = new Person('Jahid Hasan')
+console.log(p1)
